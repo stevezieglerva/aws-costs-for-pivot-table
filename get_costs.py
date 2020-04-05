05ac83fae6c43c6d2f6daf70f2190d0a1bf63950
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-04-05 09:50:21 -0400

    add formatting

--- get_costs.py
+++ get_costs.py
@@ -6,14 +6,39 @@ costs_exp = boto3.client("ce")
 
 
 def main():
-	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : "2020-01-01", "End" : "2020-04-01"}, Granularity="MONTHLY", Metrics=["BLENDED_COST"], GroupBy=[{"Type" : "DIMENSION", "Key" : "SERVICE"}, {"Type" : "DIMENSION", "Key" : "USAGE_TYPE"}])
-	print(json.dumps(costs, indent=3, default=str))
+	costs = get_costs_for_group("2020-01-01", "2020-04-01","MONTHLY", ["SERVICE", "USAGE_TYPE"] )
+	#print(json.dumps(costs, indent=3, default=str))
+
+	formatted = format_costs(costs)
+	print("Start,End,Service,Usage,Costs")
+	for line in formatted:
+		print(line)
 
 
 def get_costs_for_group(start, end, granularity, groupby):
-	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : "2020-01-01", "End" : "2020-04-01"}, Granularity="MONTHLY", Metrics=["BLENDED_COST"], GroupBy=[{"Type" : "DIMENSION", "Key" : "SERVICE"}])
-	print(json.dumps(costs, indent=3, default=str))
+	groupby_list = []
+	for grouping in groupby:
+		item = {}
+		item["Type"] = "DIMENSION"
+		item["Key"] = grouping
+		groupby_list.append(item)
+	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : start, "End" : end}, Granularity=granularity, Metrics=["BLENDED_COST"], GroupBy=groupby_list)
 	return costs
+
+def format_costs(costs):
+	lines = []
+	for time_period in costs["ResultsByTime"]:
+		start = time_period["TimePeriod"]["Start"]
+		end = time_period["TimePeriod"]["End"]
+		for group in time_period["Groups"]:
+			service = group["Keys"][0]
+			usage_type = group["Keys"][1]
+			blended_cost = group["Metrics"]["BlendedCost"]["Amount"]
+			line = f"{start},{end},\"{service}\",\"{usage_type}\",{blended_cost}"
+			lines.append(line)
+	return lines
+
+
 
 if __name__ == "__main__":
 	main()
