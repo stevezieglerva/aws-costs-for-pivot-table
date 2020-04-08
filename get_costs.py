4e491347dbd3a2a883017853fa5b54468ecc9bd4
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-04-08 15:47:00 -0400

    add support for costs by tag

--- get_costs.py
+++ get_costs.py
@@ -8,11 +8,20 @@ costs_exp = boto3.client("ce")
 def main():
 	costs = get_costs_for_group("2019-04-01", "2020-04-01","MONTHLY", ["SERVICE", "USAGE_TYPE"] )
 	#print(json.dumps(costs, indent=3, default=str))
-
 	formatted = format_costs(costs)
-	print("Start\tEnd\tService\tUsage\tCosts")
+	print("Type\tStart\tEnd\tGroup1\tGroup2\tCosts")
 	for line in formatted:
-		print(line)
+		print("Usage\t" + line)
+
+
+	tag_costs = get_costs_for_group_by_tag_type("2019-04-01", "2020-04-01","MONTHLY", "SERVICE")
+	formatted = format_costs(tag_costs)
+	#print("Start\tEnd\tService\tTag\tCosts")
+	for line in formatted:
+		print("Type\t" + line)
+
+
+
 
 
 def get_costs_for_group(start, end, granularity, groupby):
@@ -25,17 +34,30 @@ def get_costs_for_group(start, end, granularity, groupby):
 	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : start, "End" : end}, Granularity=granularity, Metrics=["BLENDED_COST"], GroupBy=groupby_list)
 	return costs
 
+def get_costs_for_group_by_tag_type(start, end, granularity, groupby_value):
+	groupby_list = []
+	item = {}
+	item["Type"] = "DIMENSION"
+	item["Key"] = groupby_value
+	groupby_list.append(item)
+	item = {}
+	item["Type"] = "TAG"
+	item["Key"] = "Type"
+	groupby_list.append(item)
+	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : start, "End" : end}, Granularity=granularity, Metrics=["BLENDED_COST"], GroupBy=groupby_list)
+	return costs
+
 def format_costs(costs):
 	lines = []
 	for time_period in costs["ResultsByTime"]:
 		start = time_period["TimePeriod"]["Start"]
 		end = time_period["TimePeriod"]["End"]
 		for group in time_period["Groups"]:
-			service = group["Keys"][0]
-			usage_type = group["Keys"][1]
+			group1 = group["Keys"][0]
+			group2 = group["Keys"][1]
 			blended_cost = group["Metrics"]["BlendedCost"]["Amount"]
 			if blended_cost != "0":
-				line = f"{start}\t{end}\t\"{service}\"\t\"{usage_type}\"\t{blended_cost}"
+				line = f"{start}\t{end}\t\"{group1}\"\t\"{group2}\"\t{blended_cost}"
 				lines.append(line)
 	return lines
 
