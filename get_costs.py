8678fc4b0e20f34d12aff3048421de5f8138697b
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-04-10 15:30:37 -0400

    refactor cost gathering

--- get_costs.py
+++ get_costs.py
@@ -6,28 +6,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 from aws import *
+from panda_helpers import *
 
 
 costs_exp = boto3.client("ce")
 
 
-START_DATE = "2019-04-01"
-END_DATE = "2020-04-01"
+MONTHLY_START_DATE = "2019-04-01"
+MONTHLY_END_DATE = "2020-04-01"
 
 def main():
 
+	get_and_write_costs_to_files()
 
-	costs = get_costs_for_group(START_DATE, END_DATE, "MONTHLY", ["SERVICE", "USAGE_TYPE"] )
-	formatted_json = json.dumps(costs, indent=3, default=str)
-##	with open("results_sample_cost.json", "w") as file:
-##		file.write(formatted_json)
-	formatted = format_costs(costs)
-	formatted_with_newlines = ["Usage\t" + i + "\n" for i in formatted]
-	with open("results.tsv", "w") as file:
-		file.write("Type\tStart\tEnd\tGroup1\tGroup2\tCosts\n")
-		file.writelines(formatted_with_newlines)
-
-	plt.style.use("seaborn")
 
 	service_usage_data = pd.read_csv("results.tsv", sep="\t")
 	service_usage_data["Start"] =  pd.to_datetime(service_usage_data["Start"], format="%Y-%m-%d")
@@ -38,6 +29,7 @@ def main():
 	with open("results_df.json", "w") as file:
 		file.write(df_json)
 
+	plt.style.use("seaborn")
 
 	top_services = group_data_by_top_and_others(service_usage_data, "Group1", 5)
 	print(top_services)
@@ -58,12 +50,23 @@ def main():
 	create_plots_for_service_usage_multicharts(service_usage_data, max_monthly_cost)
 
 
-	tag_costs = get_costs_for_group_by_tag_type(START_DATE, END_DATE, "MONTHLY", "SERVICE")
+	tag_costs = get_costs_for_group_by_tag_type(MONTHLY_START_DATE, MONTHLY_END_DATE, "MONTHLY", "SERVICE")
 	formatted = format_costs(tag_costs)
 	#print("Start\tEnd\tService\tTag\tCosts")
 ##	for line in formatted:
 ##		print("Type\t" + line)
 
+
+def get_and_write_costs_to_files():
+	costs = get_costs_for_group(MONTHLY_START_DATE, MONTHLY_END_DATE, "MONTHLY", ["SERVICE", "USAGE_TYPE"] )
+	formatted_json = json.dumps(costs, indent=3, default=str)
+	with open("results_sample_cost.json", "w") as file:
+		file.write(formatted_json)
+	formatted = format_costs(costs)
+	formatted_with_newlines = ["Usage\t" + i + "\n" for i in formatted]
+	with open("results.tsv", "w") as file:
+		file.write("Type\tStart\tEnd\tGroup1\tGroup2\tCosts\n")
+		file.writelines(formatted_with_newlines)
 
 def create_plots_for_service_multicharts(service_usage_data, max_monthly_cost):
 	top_services_list = get_top_groupings(service_usage_data, "Group1", 5)
@@ -74,7 +77,7 @@ def create_plots_for_service_multicharts(service_usage_data, max_monthly_cost):
 		print(f"\n\nService: {current_service}")
 		print(service_counts)
 		title = simplify_service_name(current_service)
-		ax = service_counts.plot(figsize=(2, 1.75), kind="line", legend=False, ylim=(0,max_monthly_cost + 2), xlim=(START_DATE, END_DATE), title=title)
+		ax = service_counts.plot(figsize=(2, 1.75), kind="line", legend=False, ylim=(0,max_monthly_cost + 2), xlim=(MONTHLY_START_DATE, MONTHLY_END_DATE), title=title)
 		ax.title.set_size(10)
 		plt.axis("off")
 		plt.savefig(f"plot_top_service_line_{count}")
@@ -89,7 +92,7 @@ def create_plots_for_service_usage_multicharts(service_usage_data, max_monthly_c
 		print(f"\n\nService Usage: {current_service}")
 		print(service_counts)
 		title = simplify_service_name(current_service)
-		ax = service_counts.plot(figsize=(2, 2.25), kind="bar", stacked=True, legend=True, ylim=(0,max_monthly_cost + 2), xlim=(START_DATE, END_DATE), title=title, color=["#0000FF", "#6495ED", "#B0C4DE"])
+		ax = service_counts.plot(figsize=(2, 2.25), kind="bar", stacked=True, legend=True, ylim=(0,max_monthly_cost + 2), xlim=(MONTHLY_START_DATE, MONTHLY_END_DATE), title=title, color=["#0000FF", "#6495ED", "#B0C4DE"])
 		box = ax.get_position()
 		ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.8])
 		ax.title.set_size(10)
@@ -100,70 +103,6 @@ def create_plots_for_service_usage_multicharts(service_usage_data, max_monthly_c
 
 
 
-
-def group_data_by_top_and_others(dataframe, column_name, size):
-	top_groupings_list = get_top_groupings(dataframe, column_name, size)
-	print(f"top_groupings: {top_groupings_list}")
-	top_groupings = dataframe[dataframe[column_name].isin(top_groupings_list)]
-	top_grouping_counts = top_groupings.groupby(by=["Start", column_name])["Costs"].sum().rename("Costs")
-
-	bottom_groupings_list =  get_bottom_groupings(dataframe, column_name, size)
-	print(f"bottom_groupings: {bottom_groupings_list}")
-	bottom_groupings = dataframe[dataframe[column_name].isin(bottom_groupings_list)]
-	bottom_grouping_counts = bottom_groupings.groupby(by=["Start"])["Costs"].sum().to_frame()
-
-	for index, row in bottom_grouping_counts.iterrows():
-		print(row)
-		start =  index
-		column_name = "Other"
-		costs = row["Costs"]
-		print(f"{start}/{column_name}/{costs}")
-		top_grouping_counts[start, column_name] = costs
-
-	print("_____")
-	print(top_grouping_counts)
-	return top_grouping_counts
-
-
-def get_single_grouping(dataframe, column, filter_value):
-	filtered_df = dataframe[dataframe[column] == filter_value]
-	grouped = filtered_df.groupby(by="Start")["Costs"].sum().to_frame()
-	return grouped
-
-
-def get_single_usage_grouping(dataframe, column, filter_value, size):
-	filtered_df = dataframe[dataframe[column] == filter_value]
-	print(filtered_df)
-	top_usages_list = get_top_groupings(filtered_df, "Group2", size)
-	print(f"top_usages_list: {top_usages_list}")
-	filtered_df = filtered_df[filtered_df["Group2"].isin(top_usages_list)]
-	grouped = filtered_df.groupby(by=["Start", "Group2"])["Costs"].sum().to_frame()
-	return grouped
-
-
-def get_top_groupings(dataframe, column_name, size, filter_column="", filter_name=""):
-	#if filter_column != "":
-	#	dataframe = dataframe[dataframe[filter_column] == filter_name]
-	dataframe = dataframe.groupby(by=[column_name])["Costs"].sum().nlargest(size).to_frame()
-	return dataframe.index.values.tolist()
-
-
-def get_bottom_groupings(dataframe, column_name, except_top_size):
-	total_groups = len(dataframe.groupby(by=[column_name])["Costs"].sum().to_frame().index)
-	bottom_size = total_groups - except_top_size
-	print(f"bottom_size: {bottom_size}")
-	groupings = dataframe.groupby(by=[column_name])["Costs"].sum().nsmallest(bottom_size).to_frame()
-	return groupings.index.values.tolist()
-
-
-def simplify_service_name(name):
-	new_name = name.replace("Amazon", "").replace("AWS", "")
-	new_name = new_name.replace("Elastic File System", "EFS")
-	new_name = new_name.replace("Elastic Load Balancing", "ELB")
-	new_name = new_name.replace("Elastic Compute Cloud", "EC2")
-	new_name = new_name.replace("Relational Database Service", "RDS")
-	new_name = new_name.replace("Simple Storage Service", "S3")
-	return new_name
 
 
 if __name__ == "__main__":
--- panda_helpers.py
+++ panda_helpers.py
@@ -2,3 +2,68 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+def group_data_by_top_and_others(dataframe, column_name, size):
+	top_groupings_list = get_top_groupings(dataframe, column_name, size)
+	print(f"top_groupings: {top_groupings_list}")
+	top_groupings = dataframe[dataframe[column_name].isin(top_groupings_list)]
+	top_grouping_counts = top_groupings.groupby(by=["Start", column_name])["Costs"].sum().rename("Costs")
+
+	bottom_groupings_list =  get_bottom_groupings(dataframe, column_name, size)
+	print(f"bottom_groupings: {bottom_groupings_list}")
+	bottom_groupings = dataframe[dataframe[column_name].isin(bottom_groupings_list)]
+	bottom_grouping_counts = bottom_groupings.groupby(by=["Start"])["Costs"].sum().to_frame()
+
+	for index, row in bottom_grouping_counts.iterrows():
+		print(row)
+		start =  index
+		column_name = "Other"
+		costs = row["Costs"]
+		print(f"{start}/{column_name}/{costs}")
+		top_grouping_counts[start, column_name] = costs
+
+	print("_____")
+	print(top_grouping_counts)
+	return top_grouping_counts
+
+
+def get_single_grouping(dataframe, column, filter_value):
+	filtered_df = dataframe[dataframe[column] == filter_value]
+	grouped = filtered_df.groupby(by="Start")["Costs"].sum().to_frame()
+	return grouped
+
+
+def get_single_usage_grouping(dataframe, column, filter_value, size):
+	filtered_df = dataframe[dataframe[column] == filter_value]
+	print(filtered_df)
+	top_usages_list = get_top_groupings(filtered_df, "Group2", size)
+	print(f"top_usages_list: {top_usages_list}")
+	filtered_df = filtered_df[filtered_df["Group2"].isin(top_usages_list)]
+	grouped = filtered_df.groupby(by=["Start", "Group2"])["Costs"].sum().to_frame()
+	return grouped
+
+
+def get_top_groupings(dataframe, column_name, size, filter_column="", filter_name=""):
+	dataframe = dataframe.groupby(by=[column_name])["Costs"].sum().nlargest(size).to_frame()
+	return dataframe.index.values.tolist()
+
+
+def get_bottom_groupings(dataframe, column_name, except_top_size):
+	total_groups = len(dataframe.groupby(by=[column_name])["Costs"].sum().to_frame().index)
+	bottom_size = total_groups - except_top_size
+	print(f"bottom_size: {bottom_size}")
+	groupings = dataframe.groupby(by=[column_name])["Costs"].sum().nsmallest(bottom_size).to_frame()
+	return groupings.index.values.tolist()
+
+
+def simplify_service_name(name):
+	new_name = name.replace("Amazon", "").replace("AWS", "")
+	new_name = new_name.replace("Elastic File System", "EFS")
+	new_name = new_name.replace("Elastic Load Balancing", "ELB")
+	new_name = new_name.replace("Elastic Compute Cloud", "EC2")
+	new_name = new_name.replace("Relational Database Service", "RDS")
+	new_name = new_name.replace("Simple Storage Service", "S3")
+	return new_name
+
+    
