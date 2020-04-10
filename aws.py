91f5db875f75bbe45c9be80e05aaae4c0213690c
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-04-10 15:18:55 -0400

    refactor aws calls to seperate module

---
+++ aws.py
@@ -0,0 +1,62 @@
+import boto3
+
+costs_exp = boto3.client("ce")
+
+def get_costs_for_group(start, end, granularity, groupby):
+	groupby_list = []
+	for grouping in groupby:
+		item = {}
+		item["Type"] = "DIMENSION"
+		item["Key"] = grouping
+		groupby_list.append(item)
+	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : start, "End" : end}, Granularity=granularity, Metrics=["BLENDED_COST"], GroupBy=groupby_list)
+	return costs
+
+
+def get_costs_for_group_by_tag_type(start, end, granularity, groupby_value):
+	groupby_list = []
+	item = {}
+	item["Type"] = "DIMENSION"
+	item["Key"] = groupby_value
+	groupby_list.append(item)
+	item = {}
+	item["Type"] = "TAG"
+	item["Key"] = "Type"
+	groupby_list.append(item)
+	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : start, "End" : end}, Granularity=granularity, Metrics=["BLENDED_COST"], GroupBy=groupby_list)
+	return costs
+
+
+def format_costs(costs):
+	lines = []
+	for time_period in costs["ResultsByTime"]:
+		start = time_period["TimePeriod"]["Start"]
+		end = time_period["TimePeriod"]["End"]
+		for group in time_period["Groups"]:
+			group1 = group["Keys"][0]
+			group2 = group["Keys"][1].replace("Type$", "")
+			blended_cost = group["Metrics"]["BlendedCost"]["Amount"]
+			if blended_cost != "0":
+				line = f"{start}\t{end}\t\"{group1}\"\t\"{group2}\"\t{blended_cost}"
+				lines.append(line)
+	return lines
+
+
+def format_dataframe_json(costs):
+	results = {}
+	for time_period in costs["ResultsByTime"]:
+		start = time_period["TimePeriod"]["Start"]
+		end = time_period["TimePeriod"]["End"]
+
+		for group in time_period["Groups"]:
+			results = update_dictionary_item_list(results, "Start", start)
+			results = update_dictionary_item_list(results, "End", end)
+
+			group1 = group["Keys"][0]
+			results = update_dictionary_item_list(results, "Group1", group1)
+			group2 = group["Keys"][1]
+			results = update_dictionary_item_list(results, "Group2", group2)
+			blended_cost = group["Metrics"]["BlendedCost"]["Amount"]
+			results = update_dictionary_item_list(results, "Costs", blended_cost)
+
+	return results
--- get_costs.py
+++ get_costs.py
@@ -1,17 +1,18 @@
 import boto3
 import json
+import calendar
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
-import calendar
+from aws import *
 
 
 costs_exp = boto3.client("ce")
 
 
-START_DATE = "2019-05-01"
-END_DATE = "2020-05-01"
+START_DATE = "2019-04-01"
+END_DATE = "2020-04-01"
 
 def main():
 
@@ -97,64 +98,7 @@ def create_plots_for_service_usage_multicharts(service_usage_data, max_monthly_c
 		plt.savefig(f"plot_top_service_usage_{count}")
 
 
-def get_costs_for_group(start, end, granularity, groupby):
-	groupby_list = []
-	for grouping in groupby:
-		item = {}
-		item["Type"] = "DIMENSION"
-		item["Key"] = grouping
-		groupby_list.append(item)
-	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : start, "End" : end}, Granularity=granularity, Metrics=["BLENDED_COST"], GroupBy=groupby_list)
-	return costs
 
-
-def get_costs_for_group_by_tag_type(start, end, granularity, groupby_value):
-	groupby_list = []
-	item = {}
-	item["Type"] = "DIMENSION"
-	item["Key"] = groupby_value
-	groupby_list.append(item)
-	item = {}
-	item["Type"] = "TAG"
-	item["Key"] = "Type"
-	groupby_list.append(item)
-	costs = costs_exp.get_cost_and_usage(TimePeriod={"Start" : start, "End" : end}, Granularity=granularity, Metrics=["BLENDED_COST"], GroupBy=groupby_list)
-	return costs
-
-
-def format_costs(costs):
-	lines = []
-	for time_period in costs["ResultsByTime"]:
-		start = time_period["TimePeriod"]["Start"]
-		end = time_period["TimePeriod"]["End"]
-		for group in time_period["Groups"]:
-			group1 = group["Keys"][0]
-			group2 = group["Keys"][1].replace("Type$", "")
-			blended_cost = group["Metrics"]["BlendedCost"]["Amount"]
-			if blended_cost != "0":
-				line = f"{start}\t{end}\t\"{group1}\"\t\"{group2}\"\t{blended_cost}"
-				lines.append(line)
-	return lines
-
-
-def format_dataframe_json(costs):
-	results = {}
-	for time_period in costs["ResultsByTime"]:
-		start = time_period["TimePeriod"]["Start"]
-		end = time_period["TimePeriod"]["End"]
-
-		for group in time_period["Groups"]:
-			results = update_dictionary_item_list(results, "Start", start)
-			results = update_dictionary_item_list(results, "End", end)
-
-			group1 = group["Keys"][0]
-			results = update_dictionary_item_list(results, "Group1", group1)
-			group2 = group["Keys"][1]
-			results = update_dictionary_item_list(results, "Group2", group2)
-			blended_cost = group["Metrics"]["BlendedCost"]["Amount"]
-			results = update_dictionary_item_list(results, "Costs", blended_cost)
-
-	return results
 
 
 def group_data_by_top_and_others(dataframe, column_name, size):
